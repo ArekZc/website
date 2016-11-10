9f605c87ce2fda58db30db5770c74d7f9879604d
Arek Zając <arekzc@gmail.com>
2016-11-10 16:55:34 +0100

    t

--- services/panel/src/command-bus/CommandBus.ts
+++ services/panel/src/command-bus/CommandBus.ts
@@ -1,21 +1,14 @@
-interface CommandHandler {
-    handle(command: any): any;
-}
-
-function Handle(handler: CommandHandler) {
+export function behaviourable() {
+    console.log("f(): evaluated");
     return function (target, propertyKey: string, descriptor: PropertyDescriptor) {
-        console.log("g(): called");
+        console.log("f(): called");
     }
 }
 
 export default class CommandBus {
-    private commands: any = {};
-
-    register(command: any): void {
-
-    }
-
     dispatch(command: any): any {
+        let Behaviour: FunctionConstructor = command.__behaviour__;
 
+        return (<any>new Behaviour()).handle(command);
     }
 }
